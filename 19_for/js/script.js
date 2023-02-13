77776f83426278afab02bcf43fc890239a46ad26
carolina-st <carolinasants7@gmail.com>
2023-02-13 11:30:45 -0300

    repeticao

--- 19_for/js/script.js
+++
@@ -1,7 +0,0 @@
-// FOR
-/**
- * Repetir alguma coisa ate chegar a conclusão que deseja
- * While - Enquanto  
- * você determina o quanto esse bloco vai ser repetido
- */
-
---
+++ 19_operadores_de_atribuição/js/script.js
@@ -0,0 +1,9 @@
+// OPERADORES DE ATRIBUIÇÃO
+/**
+ * Atribuindo valores a variável
+ * Mais utilizadas são: +=, -=, *=, /=
+ * basicamente é uma forma resumida da operação: x=x + y
+ * Em loops também é comum utilizar os operadores ++ ou --
+ * 
+ */
+
